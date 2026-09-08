d7ca5f2ccd4fdbd0c7a3fb7f29d6901a5b3ad8d5
mohamedseidmohamed583-maker <mohamedseidmohamed583@gmail.com>
2026-09-08 21:42:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -303,11 +303,11 @@ def download_video(url: str, output_template: str):
         'geo_bypass': True,
         'extractor_args': {
             'youtube': {
-                'player_client': ['android', 'web'],
+                'player_client': ['mweb', 'ios', 'android'],
             }
         },
         'http_headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1'
         }
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
