893c00a17a47fa3213b25136bcfbc371912f35ce
mohamedseidmohamed583-maker <mohamedseidmohamed583@gmail.com>
2026-09-20 13:39:36 +0300

    Change bot token in main.py
    Updated the bot token in the configuration.

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def keep_alive():
 # CONFIGURATION
 # ==================================================
 
-TOKEN = os.environ.get("8795814797:AAG-fARBC00UpldWdrdW3ViUKn0Qomrs1yI")
+TOKEN = os.environ.get("8795814797:AAHOlQUC2tZFOGu75r9yaRLa_F0Q41tzO7U")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 6753546651))
 
 # Force Join Channel
